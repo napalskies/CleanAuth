094e0ae285f30bb685d3dafe8c3c316a1881457f
adinauifi <36963895+adinauifi@users.noreply.github.com>
2025-08-28 20:14:29 +0300

    remove RefreshTokenEntity from gitignore

---
+++ src/Infrastructure/Data/Entities/RefreshTokenEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Infrastructure.Data.Entities
+{
+    public class RefreshTokenEntity
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Token { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public bool Revoked { get; set; }
+    }
+}
